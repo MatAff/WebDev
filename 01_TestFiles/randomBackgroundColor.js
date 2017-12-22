6b650a112c881adf2ce00225b78d1bfc8b82b801
MatAff <mathijsaffourtit@gmail.com>
2017-12-22 10:42:59 -0700

    Added JavaScript to randomly change the background color

---
+++ 01_TestFiles/randomBackgroundColor.js
@@ -0,0 +1,7 @@
+
+// Debug alert
+// alert("This gets run!");
+
+// Set a random background color
+var bodyElement = document.querySelector("body");
+bodyElement.style.backgroundColor = getRandomColor();
